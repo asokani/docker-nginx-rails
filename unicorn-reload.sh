32e40371ac86c41fe985acdc097e40bbd0daa5c1
Petr <webmaster@netfinity.cz>
2016-10-11 12:57:16 +0200

    unicorn reload update

--- unicorn-reload.sh
+++ unicorn-reload.sh
@@ -2,11 +2,5 @@
 
 if [ -a /var/run/unicorn.pid ]
 then
-  PID=`cat /var/run/unicorn.pid`
-  if ! kill -s USR2 $PID
-  then
-    sv restart unicorn
-  else
-    (sleep 10 && kill -s QUIT $PID) &
-  fi
+	/usr/bin/sv 2 unicorn
 fi
